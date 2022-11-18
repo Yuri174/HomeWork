c26551bf543fe3cdb27f203cd04c9f43d4fa3e6b
Имя пользователя <@pochta.com>
2022-11-18 17:48:32 +0500

    Новая задача

---
+++ Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+Console.WriteLine("Введите кординаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кординаты точки");
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Quater (int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return 1;
+    if(xc < 0 && yc > 0) return 2;
+    if(xc < 0 && yc < 0) return 3;
+    if(xc > 0 && yc < 0) return 4;
+    return 0;
+}
+
+int quater = Quater(x,y);
+string result = quater > 0 
+        ? $"Указанные кординаты соответствуют четверти ->{quater}"
+        : "Введены некорректные кординаты";
+        Console.WriteLine(result);
