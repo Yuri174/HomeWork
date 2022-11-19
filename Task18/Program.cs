6504e154b43bf2e4cbd414a4ed5123946652f80a
Имя пользователя <@pochta.com>
2022-11-19 19:23:30 +0500

    Новые задачи

---
+++ Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти(x и y).
+
+Console.WriteLine("Укажите номер четверти");
+
+string quater = Console.ReadLine();
+
+string Range(string quater1)
+{
+    if(quater1 =="1") return "x > 0  y > 0";
+    if(quater1 =="2") return "x < 0  y > 0";
+    if(quater1 =="3") return "x < 0  y < 0";
+    if(quater1 =="4") return "x > 0  y < 0";
+    return "Введены некоректные данные";
+}
+
+string range = Range(quater);
+Console.WriteLine(range);
+
+
---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+//5-> 1,4,9,16,25.
+//2->1,4
+
+int n = -1;
+while (n < 1)
+{
+Console.WriteLine("Введите число N");
+n = Convert.ToInt32(Console.ReadLine());
+if ( n < 1) Console.WriteLine("Введены неверные данные");
+}
+
+void Square(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"Число - {count} и его квадрат {count*count}");
+        count++;
+    }
+
+}
+
+Square(n);
+
